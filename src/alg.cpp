c7b8f58d005c6bf6d707df450937df02a67a5063
XeniaTklkn <tukalkinaksenia@gmail.com>
2026-02-28 11:12:41 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,7 +7,7 @@ bool checkPrime(uint64_t value) {
   if (value <= 1) return false;
   if (value == 2 || value == 3) return true;
   if (value % 2 == 0 || value % 3 == 0) return false;
-  for (uint64_t i = 5; i * i <= value; i += 2){
+  for (uint64_t i = 5; i * i <= value; i += 2) {
       if (value % i == 0) return false;
   }
   return true;
@@ -15,32 +15,32 @@ bool checkPrime(uint64_t value) {
 
 uint64_t nPrime(uint64_t n) {
     uint64_t count = 0;
-    for (uint64_t i = 1;;++i){
-        if (checkprime(i)) count++;
-        if (count == n){
+    for (uint64_t i = 1;; ++i) {
+        if (checkPrime(i)) count++;
+        if (count == n) {
             return i;
         }
     }
 }
 
 uint64_t nextPrime(uint64_t value) {
-    for (uint64_t i = value + 1;; ++i){
-        if (checkprime(i)) return i;
+    for (uint64_t i = value + 1;; ++i) {
+        if (checkPrime(i)) return i;
     }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
     uint64_t sum = 0;
-    for (uint64_t i = 1;i < hbound; ++i){
-        if (checkprime(i)) sum += i;
+    for (uint64_t i = 1; i < hbound; ++i) {
+        if (checkPrime(i)) sum += i;
     }
     return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
     uint64_t res = 0;
-    for (uint64_t i = lbound; i < hbound; ++i){
-        if (checkprime(i) && checkprime(i + 2)) res++;
+    for (uint64_t i = lbound; i < hbound; ++i) {
+        if (checkPrime(i) && checkPrime(i + 2)) res++;
     }
     return res;
 }
